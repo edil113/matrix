f769d7109ad058226ae7a161b53a1e0e47827339
edil113 <115631011+edil113@users.noreply.github.com>
2022-10-26 22:08:47 +0600

    text split

---
+++ text.py
@@ -0,0 +1,9 @@
+text = input('Введите текст: ')
+result = len(text.split())
+def add(i):
+    return 
+i = 0
+for element in text:
+    i += 1
+print('Количество символов: '+str(i))
+print(str('Количество слов: '+str (result)))
